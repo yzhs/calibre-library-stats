d0e2cec5390942b7e58659e3af65618fddf131ac
Colin Benner <colin@yzhs.de>
2017-01-31 18:42:52 +0100

    Generate a bunch of tables
    For each language, up to three tables are created: all works, long works,
    and short works.  Each of these tables shows how many such works there are,
    how many have been read, and what percentage that is, as well as the
    corresponding values for the number of pages (which is proportional to the
    number of characters) and number of words.

--- calibre_stats.jl
+++ calibre_stats.jl
@@ -36,8 +36,8 @@ const min_pages = 100;
 const available_columns = [:books, :pages, :words, :lang, :read, :readin]
 
 const column_map = Dict([(:books, "COUNT(*)"), (:pages, "SUM($pages.value)"),
-  (:words, "SUM($words.value)"), (:lang, "languages.lang_code"),
-  (:read, "$read.value"), (:readin, "$readin.value")])
+(:words, "SUM($words.value)"), (:lang, "languages.lang_code"),
+(:read, "$read.value"), (:readin, "$readin.value")])
 
 const all_stats   = "COUNT(*), SUM($pages.value), SUM(ifnull($words.value, 0))"
 const all_columns = "$all_stats, languages.lang_code, $read.value"
@@ -48,34 +48,49 @@ const is_long_work = "($words.value >= $min_words OR ($words.value IS NULL AND $
 const is_english  = "languages.lang_code = 'eng'"
 const is_german   = "languages.lang_code = 'deu'"
 
+const language = Dict(:all => "All languages", :deu => "German", :eng => "English", :jbo => "lojban")
+
+const default_from = """books
+JOIN books_languages_link    ON books.id                       = books_languages_link.book
+JOIN languages               ON books_languages_link.lang_code = languages.id
+JOIN $pages                  ON books.id                       = $pages.book
+LEFT OUTER JOIN $read        ON books.id                       = $read.book
+LEFT OUTER JOIN $words       ON books.id                       = $words.book
+LEFT OUTER JOIN $readin_link ON books.id                       = $readin_link.book
+LEFT OUTER JOIN $readin      ON $readin_link.value             = $readin.id
+"""
 
 # Construct a query string and execute the query, returning a dataframe without
 # nullable columns
-function query(columns::Array{Symbol, 1}, condition::String, group_by::String)
+function query(columns   :: Array{Symbol, 1},
+               condition :: Union{String, Array{String}},
+               group_by  :: Union{String, Array{String}})
   nullable_to_na(x) = isnull(x) ? NA : get(x)
-  select = join(map(x -> column_map[x], columns), ", ")
-  if !isempty(group_by)
-    group_by = "GROUP BY " * group_by
-  end
-  if !isempty(condition)
-    condition = "WHERE " * condition
-  end
+  select = map(x -> column_map[x], columns)
 
-  query_string = """SELECT $select
-  FROM books
-  JOIN books_languages_link    ON books.id                       = books_languages_link.book
-  JOIN languages               ON books_languages_link.lang_code = languages.id
-  JOIN $pages                  ON books.id                       = $pages.book
-  LEFT OUTER JOIN $read        ON books.id                       = $read.book
-  LEFT OUTER JOIN $words       ON books.id                       = $words.book
-  LEFT OUTER JOIN $readin_link ON books.id                       = $readin_link.book
-  LEFT OUTER JOIN $readin      ON $readin_link.value             = $readin.id
-  $condition
-  $group_by
-  ORDER BY languages.lang_code ASC
-  ;"""
+  # Build the query string
+  query_string = "SELECT " * join(select, ", ")
+  query_string *= " FROM " * default_from
+  if !isempty(condition)
+    query_string *= " WHERE " *
+    if typeof(condition) == String
+      condition
+    else
+      join(condition, " AND ")
+    end
+  end
+  if typeof(group_by) != String
+    group_by = join(group_by, ", ")
+  end
+  if !isempty(group_by)
+    query_string *= " GROUP BY " * group_by
+  end
+  query_string *= " ORDER BY languages.lang_code ASC"
+
+  # and execute the query
   foo = SQLite.query(db, query_string)
 
+  # Polish the resulting DataFrame
   names!(foo, columns)
   for col in columns
     foo[col] = convert(Array, map(nullable_to_na, foo[col]))
@@ -83,8 +98,8 @@ function query(columns::Array{Symbol, 1}, condition::String, group_by::String)
   foo
 end
 
-query(str::String) = query(available_columns, str, "$read.value, languages.lang_code")
-query() = query("")
+query(str::Union{String, Array{String}}) = query(available_columns, str, ["$read.value", "languages.lang_code"])
+query() = query(String[])
 
 
 #
@@ -94,18 +109,44 @@ all_works   = query()
 long_works  = query(is_long_work)
 short_works = query(is_short_work)
 
-read_short_works = @where(short_works, !isna(:read))
+english_books = @where(long_works, :lang .== "eng")
+german_books = @where(long_works, :lang .== "deu")
+
 
 total_books = sum(all_works[:books])
 total_pages = sum(all_works[:pages])
 total_words = sum(all_works[:words])
 
+read_all_works = @where(all_works, !isna(:read))
+
+#             length  read  language
+#               |       |     |
+#               v       v     v
+works = Dict((:all,   :all, :all) => all_works,
+             (:long,  :all, :all) => long_works,
+             (:short, :all, :all) => short_works)
+
+all_languages = sort(union(map(x -> convert(Symbol, x), all_works[:lang])))
+
+read_statuses = [:all]
+
+for length in [:all, :long, :short]
+    works[(length, :read, :all)] = @where(works[(length, :all, :all)], !isna(:read))
+  for lang in all_languages
+    works[(length, :all, lang)] = @where(works[(length, :all, :all)], :lang .== string(lang))
+    works[(length, :read, lang)] = @where(works[(length, :all, lang)], !isna(:read))
+  end
+end
+
+
+read_short_works = @where(short_works, !isna(:read))
 read_long_works = long_works[!long_works[:read].na, :]
 
 df = read_long_works
 read_english_books = @where(read_long_works, :lang .== "eng", :read .== 1)
-read_german_books  = @where(read_long_works, (:lang .== "deu") | (:read .== 0))
-read_german_books  = DataFrame(books = sum(read_german_books[:books]), pages = sum(read_german_books[:pages]), words = sum(read_german_books[:words]))
+read_german_books = @where(read_long_works, :lang .== "deu", :read .== 1)
+read_in_german  = @where(read_long_works, (:lang .== "deu") | (:read .== 0))
+read_in_german  = DataFrame(books = sum(read_in_german[:books]), pages = sum(read_in_german[:pages]), words = sum(read_in_german[:words]))
 read_short_works = DataFrame(books = sum(read_short_works[:books]), pages = sum(read_short_works[:pages]), words = sum(read_short_works[:words]))
 
 
@@ -128,8 +169,42 @@ total_read = sum(convert(Array, all_works[!all_works[:read].na, [:books, :pages,
 # Generate a simple report
 #
 # Group digits
-f(x::DataArrays.DataArray) = format(x[1], commas=true)
+f(language::Symbol, length::Symbol) = (read::Symbol, column::Symbol) ->
+  format(sum(works[(length, read, language)][column]), commas=true)
+f(df::DataFrame, s::Symbol) = format(sum(df[s]), commas=true)
+f(x::DataArrays.DataArray) = format(sum(x), commas=true) # TODO make sure there is only one row
 f(x::Integer) = format(x, commas=true)
+
+function percent(needle::DataFrame, haystack::DataFrame, column::Symbol)
+  return format(sum(needle[column]) * (100.0::Real) / sum(haystack[column]), precision=1) * "%"
+end
+
+function render_table(lang::Symbol, length::Symbol)
+  if size(works[(length, :read, lang)], 1) == 0
+    return ""
+  end
+  g = f(lang, length)
+  percnt(column::Symbol) = percent(works[(length, :read, lang)], works[(length, :all, lang)], column)
+  result = ""
+  if length == :short
+    result *= "### Short Works"
+  elseif length == :long
+    result *= "### Long Works"
+  end
+  result * """
+
+  | Status    | Books               | Pages               | Words               |
+  | :-------  | ------------------: | ------------------: | ------------------: |
+  | All       | $(g(:all, :books))  | $(g(:all, :pages))  | $(g(:all, :words))  |
+  | Read      | $(g(:read, :books)) | $(g(:read, :pages)) | $(g(:read, :words)) |
+  | *Percent* | *$(percnt(:books))* | *$(percnt(:pages))* | *$(percnt(:words))* |
+  """
+end
+
+function bullet(language::Symbol)
+  "* "
+end
+
 
 tmpl = """My (electronic) library currently contains a total of $total_books
 pieces of writing (novels, novellas, short stories, text books, etc.) with an
@@ -140,16 +215,35 @@ files.
 
 Of these I have read
 
-* $(f(read_english_books[:books])) books, $(f(read_english_books[:pages])) pages,
-  $(f(read_english_books[:words])) words in English;
-* $(f(read_german_books[:books])) books, $(f(read_german_books[:pages])) pages,
-  $(f(read_german_books[:words])) words in German; and
-* $(f(read_short_works[:books])) works of less than $(f(min_words)) words in English,
-  totalling $(f(read_short_works[:pages])) pages, and $(f(read_short_works[:words])) words.
+* $(f(read_english_books[:books])) books ($(percent(read_english_books, english_books, :books))),
+  $(f(read_english_books[:pages])) pages ($(percent(read_english_books, english_books, :pages))),
+  $(f(read_english_books[:words])) words in English ($(percent(read_english_books, english_books, :words)));
+* $(f(read_in_german[:books])) books ($(percent(read_german_books, german_books, :books))),
+  $(f(read_in_german[:pages])) pages ($(percent(read_german_books, german_books, :pages))),
+  $(f(read_in_german[:words])) words in German ($(percent(read_german_books, german_books, :words)));
+  and
+* $(f(read_short_works[:books])) works ($(percent(read_short_works, short_works, :books)))
+  of less than $(f(min_words)) words in English, totalling $(f(read_short_works[:pages])) pages
+  ($(percent(read_short_works, short_works, :pages))), and $(f(read_short_works[:words])) words
+  ($(percent(read_short_works, short_works, :words))).
 
-Thus, in total, I have read $(f(total_read[1])) works consisting of
-$(f(total_read[2])) pages and $(f(total_read[3])) words.
+Thus, in total, I have read $(f(total_read[1])) works ($(percent(read_all_works, all_works, :books)))
+consisting of $(f(total_read[2])) pages ($(percent(read_all_works, all_works, :pages)))
+and $(f(total_read[3])) words ($(percent(read_all_works, all_works, :words))).
+
 """
+
+for lang in vcat([:all], all_languages)
+  foo = """
+  $(render_table(lang, :all))
+  $(render_table(lang, :long))
+  $(render_table(lang, :short))
+
+  """
+  if length(foo) > 10
+    tmpl *= "## " * language[lang] * foo
+  end
+end
 
 output = Base.Markdown.parse(tmpl);
 
@@ -167,6 +261,13 @@ close(file)
 
 display(output)
 
+
+#
+# Write the set of all words (whitespace separated strings) from all the books
+# I read to a file.
+#
+
+### searchdir(path, key) = filter(x -> contains(x, key), readdir(path))
 ### function full_path_to_txt(rel_path::String)
 ###   base_path = "/home/joghurt/books/non-fiction/"
 ###   tmp = searchdir(base_path * rel_path, ".txt")
