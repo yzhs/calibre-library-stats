2f78fd188363340e95f13e677e81731cdce11c36
Colin Benner <colin@yzhs.de>
2018-05-04 09:48:40 +0200

    Add plotting functions; add duration histogram

--- range_plot.py
+++ range_plot.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
 import pandas.tseries.converter as converter
 
 dropna = True
+savefig_dpi = 130
 
 pd.set_option('display.line_width', 120)
 pd.set_option('display.max_columns', 20)
@@ -17,8 +19,8 @@ def error_bars(df):
     return np.vstack((zeros, df.Duration.values))
 
 
-def ceil(td):
-    return pd.Timedelta(td).ceil('D')
+def ceil(td, roundto='D'):
+    return pd.Timedelta(td).ceil(roundto)
 
 
 def get_data():
@@ -35,16 +37,75 @@ def get_data():
     return df
 
 
+def allocate_ys(df):
+    n = df.Start.size
+    result = []
+
+    max_register = 0
+    registers = {}
+
+    # Greedily allocate registers
+    for i in range(n):
+        for j in range(max_register+1):
+            if j not in registers:
+                registers[j] = i
+                result.append(j)
+                if j == max_register:
+                    max_register += 1
+                break
+            else:
+                if df.End.values[registers[j]] < df.Start.values[i]:
+                    registers[j] = i
+                    result.append(j)
+                    break
+
+    return np.array(result)
+
+
+def plot_bars(df, fiction, nonfiction):
+    line_height = 4
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ys = allocate_ys(df)
+
+    ax.errorbar(x=fiction.Start.values,
+                y=ys[df['IsFiction']],
+                xerr=error_bars(fiction),
+                linestyle='None', elinewidth=line_height)
+
+    ax.errorbar(x=nonfiction.Start.values,
+                y=ys[df['IsFiction'] == False],
+                xerr=error_bars(nonfiction),
+                linestyle='None', elinewidth=line_height)
+
+    plt.savefig('bars.png', dpi=savefig_dpi)
+
+
+def plot_histogram(df, fiction, nonfiction):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.hist([np.array(fiction['Duration'].map(lambda x: x.days),
+                      dtype='float64'),
+             np.array(nonfiction['Duration'].map(lambda x: x.days),
+                      dtype='float64')],
+            histtype='barstacked',
+            bins=list(range(-7, 1764, 14)))
+
+    plt.title('Number of days spent reading a book')
+    blue_patch = mpatches.Patch(label='Fiction titles')
+    orange_patch = mpatches.Patch(color='orange',
+                                  label='Nonfiction titles')
+    plt.legend(handles=[blue_patch, orange_patch])
+
+    plt.savefig('histogram.png', dpi=savefig_dpi)
+
+
 df = get_data()
+df.sort_values('Start', inplace=True)
 fiction = df[df['IsFiction']]
 nonfiction = df[df['IsFiction'] == False]
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ys = np.array(range(fiction.Start.size))
-ax.errorbar(x=fiction.Start.values, y=ys, xerr=error_bars(fiction),
-            linestyle='None', elinewidth=2)
-ys = np.array(range(nonfiction.Start.size))
-ax.errorbar(x=nonfiction.Start.values, y=ys, xerr=error_bars(nonfiction),
-            linestyle='None', elinewidth=2)
-plt.show()
+plot_bars(df, fiction, nonfiction)
+plot_histogram(df, fiction, nonfiction)
